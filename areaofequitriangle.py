e5a7681cacedfeda0dd0cd30d29ed1bdfe12a6ce
Nithya Sri P R <122468951+nithyasri7846@users.noreply.github.com>
2025-05-13 19:39:37 +0530

    Create areaofequitriangle.py

---
+++ areaofequitriangle.py
@@ -0,0 +1,19 @@
+"""The area of an equilateral triangle is ¼(√3a2) where "a" represents a side of the triangle. You are provided with the side "a". Find the area of the equilateral triangle.
+
+
+Input Description:
+The side of an equilateral triangle is provided as the input.
+
+Output Description:
+Find the area of the equilateral triangle and print the answer up to 2 decimal places after rounding off.
+
+Sample Input :
+20
+Sample Output :
+173.21"""
+
+import math
+
+a = float(input())
+area = (math.sqrt(3) / 4) * (a ** 2)
+print(f"{area:.2f}")
